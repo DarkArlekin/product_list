4c816cb813699f391156d136b1c2b824233b7726
DarkFoxIt <nikolaygaginua@gmail.com>
2022-05-14 00:09:19 +0300

    refactor product repo

---
+++ lib/products/pages/products_list.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class ProductList extends StatelessWidget {
+  const ProductList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Products',
+            style: Theme.of(context).textTheme.headline2,
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/products/pages/products_page.dart
+++ lib/products/pages/products_page.dart
@@ -1,28 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:product_list/app/widgets/app_bar.dart';
 import 'package:product_list/app/widgets/app_drawer.dart';
+import 'products_list.dart';
 
 class ProductsPage extends StatelessWidget {
   const ProductsPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: const PreferredSize(
+    return const Scaffold(
+      appBar: PreferredSize(
           child: MainAppBar(), preferredSize: Size(double.infinity, 60)),
-      body: Container(
-        alignment: Alignment.center,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Products',
-              style: Theme.of(context).textTheme.headline2,
-            ),
-          ],
-        ),
-      ),
-      drawer: const AppDrawer(),
+      body: ProductList(),
+      drawer: AppDrawer(),
       extendBodyBehindAppBar: true,
     );
   }
